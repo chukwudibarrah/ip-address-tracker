fd3ccd89b31d6ac9de656a1089391b4157916c4c
cbarrah <118562380+chukwudibarrah@users.noreply.github.com>
2023-04-19 18:45:55 +0100

    Updated map api to show map on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
 $(document).ready(function () {
 
-  let success = (position) => {
-    let lat = position.coords.latitude
-    let long = position.coords.longitude
+  // let success = (position) => {
+  //   let lat = position.coords.latitude
+  //   let long = position.coords.longitude
 
-    console.log(lat, long);
+  //   // console.log(lat, long);
 
-    let map = L.map("map").setView([lat, long], 15);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
+  //   let map = L.map("map").setView([lat, long], 15);
+  //   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  //     maxZoom: 19,
+  //     attribution:
+  //       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  //   }).addTo(map);
 
-    L.marker([lat, long]).addTo(map);
-  }
+  //   L.marker([lat, long]).addTo(map);
+  // }
 
-  let error = () => {
-    console.log('Unable to get location');
-  }
+  // let error = () => {
+  //   console.log('Unable to get location');
+  // }
 
-  navigator.geolocation.watchPosition(success, error)
-
-
-
-
+  // navigator.geolocation.watchPosition(success, error)
   
+  function clearResults() {
+    $('.result-display').empty();
+  }
 
 
   $("#form").on("submit", function (e) {
     e.preventDefault(); //prevent default form behaviour
+    clearResults()
 
     // define and get user input from field
     let userInput = $(".user-input").val().trim();
@@ -45,8 +45,6 @@ $(document).ready(function () {
       return;
     }
 
-    console.log("The user input is", userInput);
-
     // make map api call
     $.ajax({
       url: queryURL,
@@ -54,17 +52,37 @@ $(document).ready(function () {
     }).then(function (response) {
       let displayIp = response.ip;
       let displayCity = response.location.city;
-      let lat = response.location.lat;
-      let long = response.location.lng;
+      let newLat = response.location.lat;
+      let newLong = response.location.lng;
       let timezone = response.location.timezone;
       let isp = response.isp;
-      console.log("The city is", displayCity);
-      let map = L.map("map").setView([lat, long], 13);
+
+      // display the results on the results bar
+
+      $('#show-ip').append($('<p>').addClass("result-display text-lg font-medium text-[#2B2B2B] py-2").text(displayIp));
+      
+      $('#show-location').append($('<p>').addClass("result-display text-lg font-medium text-[#2B2B2B] py-2").text(displayCity));
+
+      $('#show-timezone').append($('<p>').addClass("result-display text-lg font-medium text-[#2B2B2B] py-2").text(timezone));
+
+      $('#show-isp').append($('<p>').addClass("result-display text-lg font-medium text-[#2B2B2B] py-2").text(isp));
+
+      // pass values to the map API
+
+      
+      let map = L.map("map").setView([newLat, newLong], 17);
       L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }).addTo(map);
+      L.marker([newLat, newLong]).addTo(map);
+      map.panTo([newLat, newLong]);
+
+      // pass values to and create results fields
+
+
+
     });
 
     $(".user-input").val("");
